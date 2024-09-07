0d7815c51204d357db709ceee0a5774e9b4bcaef
mdrezuanislamridoy <mdrezuanislamridoy@gmail.com>
2024-09-07 11:45:20 +0600

    update

--- Ridoy/day-1/script.js
+++ Ridoy/day-1/script.js
@@ -6,3 +6,35 @@ anime.addEventListener('mouseover',()=>{
 anime.addEventListener('mouseout',()=>{
   anime.style.backgroundColor='blue'
 })
+
+
+
+// expandable nav bar
+
+const ebar = document.querySelector('#ebars');
+
+const elist = document.querySelectorAll('.elist')
+const etext = document.querySelectorAll('.elist a')
+
+let etoggle = false;
+
+ebar.addEventListener('click',()=>{
+  etoggle=!etoggle;
+  if (etoggle) {
+    elist.forEach(e=>{
+      e.style.width='25px'
+      
+    })
+    etext.forEach(e=>{
+      e.style.display='none'
+    })
+  }else{
+    elist.forEach(e=>{
+      e.style.width='100%'
+      
+    })
+    etext.forEach(e=>{
+      e.style.display='inline'
+    })
+  }
+})
